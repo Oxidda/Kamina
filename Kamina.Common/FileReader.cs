1d5bf144a60ccb8f92b457c3cbea311452a0d22d
jasper <gamesfreak16@hotmail.com>
2017-02-09 00:58:03 +0100

    .

--- Kamina.Common/FileReader.cs
+++ Kamina.Common/FileReader.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.IO.IsolatedStorage;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kamina.Common
 {
