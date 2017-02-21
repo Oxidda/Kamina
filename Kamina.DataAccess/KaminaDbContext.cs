d90ab7fde4dfc4561090d8d204a9289e52506084
jasper <gamesfreak16@hotmail.com>
2017-02-21 19:50:18 +0100

    a

--- Kamina.DataAccess/KaminaDbContext.cs
+++ Kamina.DataAccess/KaminaDbContext.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Kamina.DataAccess
 {
-    class KaminaDbContext
+    public class KaminaDbContext : DbContext
     {
+        public KaminaDbContext()
+        {
+            
+        }
     }
 }
